30733d99f451778245901adeb3f9e176627d7d21
toniharju90 <toni.harju2@metropolia.fi>
2025-08-31 12:18:41 +0300

    Tehtävä 2.2

--- Tehtävä 2.2.py
+++ Tehtävä 2.2.py
@@ -1,3 +1,11 @@
-# Kysytään käyttäjän nimeä
-nimi = input("Anna nimesi: ")
 
+import math
+
+# Kysy ympyrän säde
+sade = float(input("Anna ympyrän säde: "))
+
+# Laske pinta-ala
+pinta_ala = math.pi * sade**2
+
+
+print("Ympyrän pinta-ala on:", pinta_ala)
