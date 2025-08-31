8259e73fb451915d999d732574d6fb0efff8e4e1
toniharju90 <toni.harju2@metropolia.fi>
2025-08-31 12:57:09 +0300

    Tehtävä 2.4

---
+++ Tehtävä 2.4.py
@@ -0,0 +1,22 @@
+
+luku1 = int(input("Anna ensimmäinen luku: "))
+
+
+luku2 = int(input("Anna toinen luku: "))
+
+
+luku3 = int(input("Anna kolmas luku: "))
+
+
+summa = luku1 + luku2 + luku3
+
+tulo = luku1 * luku2 * luku3
+
+keskiarvo = summa / 3
+
+
+print("Lukujen summa on:", summa)
+
+print("Lukujen tulo on:", tulo)
+
+print("Lukujen keskiarvo on:", keskiarvo)
